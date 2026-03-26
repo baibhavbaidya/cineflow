3f9aace17b8f522a4ade8f60228321a680366ec8
Baibhav Baidya <baibhavbaidya@gmail.com>
2026-03-27 01:48:18 +0530

    fix: distinct movies in API, updated README

--- dashboard/app/api/movies/route.ts
+++ dashboard/app/api/movies/route.ts
@@ -4,21 +4,21 @@ import sql from "@/lib/db";
 export async function GET() {
   try {
     const movies = await sql`
-      SELECT
-        movie_id,
-        title,
-        release_date,
-        release_year,
-        popularity,
-        vote_average,
-        vote_count,
-        original_language,
-        poster_path,
-        genre_name
-      FROM fct_trending_movies
-      ORDER BY popularity DESC
-      LIMIT 20
-    `;
+  SELECT DISTINCT ON (movie_id)
+    movie_id,
+    title,
+    release_date,
+    release_year,
+    popularity,
+    vote_average,
+    vote_count,
+    original_language,
+    poster_path,
+    genre_name
+  FROM fct_trending_movies
+  ORDER BY movie_id, popularity DESC
+  LIMIT 20
+`;
     return NextResponse.json(movies);
   } catch (error) {
     return NextResponse.json({ error: "Failed to fetch movies" }, { status: 500 });
